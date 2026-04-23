9308db395aa92807ebf7183e80b8052bed959c04
Daniel Olarte <daniol1618@gmail.com>
2026-04-23 14:42:36 -0500

    chore: initial project backbone

---
+++ src/main/java/com/api/productengine/ProductengineApplication.java
@@ -0,0 +1,13 @@
+package com.api.productengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductengineApplication.class, args);
+	}
+
+}
